853ee88b15755fb5d3712eabf19b3b5c67ce32c8
Romashkashtyr <Romainshtyr@yandex.ru>
2024-05-08 14:48:25 +0300

    Room's logic

--- app/src/main/java/com/romashka/catviewer/catadapter/CatDataCallback.kt
+++ app/src/main/java/com/romashka/catviewer/catadapter/CatDataCallback.kt
@@ -2,13 +2,15 @@ package com.romashka.catviewer.catadapter
 
 import androidx.recyclerview.widget.DiffUtil
 import com.romashka.catviewer.domain.model.CatData
+import com.romashka.catviewer.room.datainterfaces.CatDataForRoom
 
-class CatDataCallback() : DiffUtil.ItemCallback<CatData>() {
-    override fun areItemsTheSame(oldItem: CatData, newItem: CatData): Boolean {
-        TODO("Not yet implemented")
+class CatDataCallback() : DiffUtil.ItemCallback<CatDataForRoom>() {
+
+    override fun areItemsTheSame(oldItem: CatDataForRoom, newItem: CatDataForRoom): Boolean {
+        return oldItem.url == newItem.url && oldItem.fact == newItem.fact
     }
 
-    override fun areContentsTheSame(oldItem: CatData, newItem: CatData): Boolean {
-        TODO("Not yet implemented")
+    override fun areContentsTheSame(oldItem: CatDataForRoom, newItem: CatDataForRoom): Boolean {
+        return oldItem == newItem
     }
 }
